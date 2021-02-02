b7b568435e8008deba8713552bd5f07fb92f5f73
Thomas Rainford <thomas@rainfords.net>
2021-02-02 15:26:25 +1300

    Add new entities to mikro-orm config.

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,9 +1,11 @@
 import { MikroORM } from "@mikro-orm/core"
 import { MongoHighlighter } from "@mikro-orm/mongo-highlighter"
+import { Collection } from "./entities/Collection"
+import { NotesList } from "./entities/NotesList"
 import { User } from "./entities/User"
 
 export default {
-   entities: [User],
+   entities: [User, Collection, NotesList],
    dbName: 'markdown-notes-db',
    type: 'mongo',
    clientUrl: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`,
