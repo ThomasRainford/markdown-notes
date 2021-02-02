2b89dfcfab9258cd6a49c178de4065cad1733062
Thomas Rainford <thomas@rainfords.net>
2021-02-02 14:12:07 +1300

    Update mikro-orm config for new database.

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -4,7 +4,7 @@ import { User } from "./entities/User"
 
 export default {
    entities: [User],
-   dbName: 'test-db-name',
+   dbName: 'markdown-notes-db',
    type: 'mongo',
    clientUrl: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`,
    highlighter: new MongoHighlighter(),
