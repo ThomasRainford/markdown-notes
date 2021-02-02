a7e2f66cdeeda343ddae1268b1cd11f80e5fdcd9
Thomas Rainford <thomas@rainfords.net>
2021-02-03 11:36:02 +1300

    Add query for getting one collection.

--- server/src/resolvers/collection.ts
+++ server/src/resolvers/collection.ts
@@ -1,15 +1,17 @@
 import { User } from "../entities/User";
 import { OrmContext } from "../types/types";
-import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import { Collection } from "../entities/Collection";
 import { CollectionResponse } from "./object-types/CollectionResponse";
 import { validateVisibility } from "../utils/validateVisibility";
 import { validateTitle } from "../utils/validateTitle";
+import { isAuth } from "../middleware/isAuth";
 
 @Resolver(Collection)
 export class CollectionResolver {
 
    @Mutation(() => CollectionResponse)
+   @UseMiddleware(isAuth)
    async createCollection(
       @Arg('title') title: string,
       @Arg('visibility') visibility: string,
@@ -45,6 +47,30 @@ export class CollectionResolver {
 
       return { collection }
 
+   }
+
+   @Query(() => CollectionResponse)
+   @UseMiddleware(isAuth)
+   async collection(
+      @Ctx() { em }: OrmContext,
+      @Arg('id', { nullable: true }) id?: string,
+      @Arg('title', { nullable: true }) title?: string
+   ): Promise<CollectionResponse> {
+
+      const repo = em.getRepository(Collection)
+
+      const collection = await repo.findOne({ $or: [{ title }, { id }] })
+
+      if (!collection) {
+         return {
+            error: {
+               property: 'collection',
+               message: 'Collection does not exisit.'
+            }
+         }
+      }
+
+      return { collection }
    }
 
 }
