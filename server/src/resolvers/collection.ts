6e0e425ac7072d749712ce37a45b09fbd8012843
Thomas Rainford <thomas@rainfords.net>
2021-02-02 14:06:38 +1300

    Add resolver files.

---
+++ server/src/resolvers/collection.ts
@@ -0,0 +1,7 @@
+import { Resolver } from "type-graphql";
+import { Collection } from "../entities/Collection";
+
+@Resolver(Collection)
+export class CollectionResolver {
+
+}
---
+++ server/src/resolvers/notesList.ts
@@ -0,0 +1,7 @@
+import { NotesList } from "../entities/NotesList";
+import { Resolver } from "type-graphql";
+
+@Resolver(NotesList)
+export class NotesListResolver {
+
+}
