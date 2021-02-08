25433fcf91833a5153f4bebdd3331f5361c112a6
Thomas Rainford <thomas@rainfords.net>
2021-02-09 10:54:53 +1300

    Create hook for checking user is authenticted before page is rendered.

---
+++ client/utils/useIsAuth.ts
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { UseQueryState } from "urql"
+import { MeQuery } from "../generated/graphql"
+
+export const useIsAuth = (user: UseQueryState<MeQuery, object>) => {
+
+   const router = useRouter()
+
+   useEffect(() => {
+      console.log('isAuth: ', user)
+      // Check if user is logged.
+      if (!user.fetching && !user.data?.me) {
+         router.replace('/account/login')
+      }
+   }, [user, router])
+}
