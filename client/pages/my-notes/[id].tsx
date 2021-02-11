b510114705af17d530f588ddaea69445de80d9d9
Thomas Rainford <thomas@rainfords.net>
2021-02-12 11:02:44 +1300

    Add layout to my-note page component.

--- client/pages/my-notes/[id].tsx
+++ client/pages/my-notes/[id].tsx
@@ -1,7 +1,11 @@
 import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Flex } from '@chakra-ui/react'
+import { withUrqlClient } from 'next-urql'
 import { useRouter } from 'next/router'
 import React from 'react'
+import MyNotesPageLayout from '../../components/my-notes/MyNotesPageLayout'
+import PageLoadingIndicator from '../../components/PageLoadingIndicator'
 import { useMeQuery } from '../../generated/graphql'
+import { createUrqlClient } from '../../utils/createUrqlClient'
 import { useIsAuth } from '../../utils/useIsAuth'
 
 interface Props {
@@ -19,26 +23,19 @@ const MyNotes = ({ }) => {
    console.log(router.query.id)
 
    return (
-      <Flex>
-         {/*
-            - Use one side menu.
-            - Collection will be Chakra Accordians.
-            - Inside the collections are the lists which will also be Accordians (hopefully).
-            - Inside the list Accordians are the notes.
-            - Need to hightlight currently selected collection and list Accordian.
-            - Below is a successful test of implementing Accordian components inside an Accordian component.
-         */}
-         <Accordion allowMultiple>
-            <AccordionItem>
-               <h2>
-                  <AccordionButton>
-                     <Box flex="1" textAlign="left">
-                        Section 1 title
-                     </Box>
-                     <AccordionIcon />
-                  </AccordionButton>
-               </h2>
-               <AccordionPanel pb={4}>
+      <>
+         { !user.fetching && user.data?.me
+            ?
+            <MyNotesPageLayout user={user}>
+               <Flex>
+                  {/*
+               - Use one side menu.
+               - Collection will be Chakra Accordians.
+               - Inside the collections are the lists which will also be Accordians (hopefully).
+               - Inside the list Accordians are the notes.
+               - Need to hightlight currently selected collection and list Accordian.
+               - Below is a successful test of implementing Accordian components inside an Accordian component.
+               */}
                   <Accordion allowMultiple>
                      <AccordionItem>
                         <h2>
@@ -50,11 +47,42 @@ const MyNotes = ({ }) => {
                            </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                           veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                           commodo consequat.
-                          </AccordionPanel>
+                           <Accordion allowMultiple>
+                              <AccordionItem>
+                                 <h2>
+                                    <AccordionButton>
+                                       <Box flex="1" textAlign="left">
+                                          Section 1 title
+                                       </Box>
+                                       <AccordionIcon />
+                                    </AccordionButton>
+                                 </h2>
+                                 <AccordionPanel pb={4}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.
+                                 </AccordionPanel>
+                              </AccordionItem>
+
+                              <AccordionItem>
+                                 <h2>
+                                    <AccordionButton>
+                                       <Box flex="1" textAlign="left">
+                                          Section 2 title
+                              </Box>
+                                       <AccordionIcon />
+                                    </AccordionButton>
+                                 </h2>
+                                 <AccordionPanel pb={4}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.
+                                 </AccordionPanel>
+                              </AccordionItem>
+                           </Accordion>
+                        </AccordionPanel>
                      </AccordionItem>
 
                      <AccordionItem>
@@ -74,28 +102,13 @@ const MyNotes = ({ }) => {
                         </AccordionPanel>
                      </AccordionItem>
                   </Accordion>
-               </AccordionPanel>
-            </AccordionItem>
-
-            <AccordionItem>
-               <h2>
-                  <AccordionButton>
-                     <Box flex="1" textAlign="left">
-                        Section 2 title
-                     </Box>
-                     <AccordionIcon />
-                  </AccordionButton>
-               </h2>
-               <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat.
-               </AccordionPanel>
-            </AccordionItem>
-         </Accordion>
-      </Flex>
+               </Flex>
+            </MyNotesPageLayout>
+            :
+            <PageLoadingIndicator />
+         }
+      </>
    )
 }
 
-export default MyNotes
+export default withUrqlClient(createUrqlClient)(MyNotes)
