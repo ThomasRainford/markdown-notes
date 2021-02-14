32abc4633123d6b2c6ec36b3e1d679fa9c19672f
Thomas Rainford <thomas@rainfords.net>
2021-02-15 10:40:49 +1300

    Add tooltip to activity collection items.

--- client/components/activity/CollectionItem.tsx
+++ client/components/activity/CollectionItem.tsx
@@ -1,4 +1,4 @@
-import { Center, Divider, Flex, Icon, ListIcon, ListItem, Text } from '@chakra-ui/react'
+import { Center, Divider, Flex, Icon, ListIcon, ListItem, Text, Tooltip } from '@chakra-ui/react'
 import React from 'react'
 import { MdLock, MdLockOpen, MdThumbUp } from 'react-icons/md'
 import { Collection } from '../../generated/graphql'
@@ -11,13 +11,17 @@ interface Props {
 const CollectionItem: React.FC<Props> = ({ collection }) => {
    return (
       <ListItem key={collection.id}>
-         <Flex direction="column" bg="#8EE4AF" p="0.5em" border="1px" borderColor="#EDF5E1" borderRadius="md">
-            <Flex align="center">
-               <ListIcon as={collection.visibility === 'public' ? MdLockOpen : MdLock} />
-               <Text fontSize="lg" fontWeight="bold" mb="0.25em" ml="0.5em">{collection.title}</Text>
+         <Tooltip hasArrow label="Open Collection" bg="#379683">
+            <Flex direction="column" bg="#8EE4AF" p="0.5em" border="1px" borderColor="#EDF5E1" borderRadius="md"
+               _hover={{ borderColor: "#05386B" }}
+            >
+               <Flex align="center">
+                  <ListIcon as={collection.visibility === 'public' ? MdLockOpen : MdLock} />
+                  <Text fontSize="lg" fontWeight="bold" mb="0.25em" ml="0.5em">{collection.title}</Text>
+               </Flex>
+               <CollectionInfo collection={collection} />
             </Flex>
-            <CollectionInfo collection={collection} />
-         </Flex>
+         </Tooltip>
       </ListItem>
    )
 }
