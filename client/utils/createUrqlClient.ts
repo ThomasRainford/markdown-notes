840c9a99fb469bacfca8b5e40ca14b67d8361242
Thomas Rainford <thomas@rainfords.net>
2021-02-15 11:42:19 +1300

    Invalidate collections query when saving collections.

--- client/utils/createUrqlClient.ts
+++ client/utils/createUrqlClient.ts
@@ -17,6 +17,13 @@ const invalidateMe = (cache: Cache) => {
    })
 }
 
+const invalidateCollections = (cache: Cache) => {
+   const allFields = cache.inspectFields('Query')
+   const fieldInfos = allFields.filter((info) => info.fieldName === 'collections')
+   fieldInfos.forEach((fi) => {
+      cache.invalidate('Query', 'collections', fi.arguments || null)
+   })
+}
 
 export const createUrqlClient = (ssrExchange: any) => {
    return {
@@ -29,8 +36,11 @@ export const createUrqlClient = (ssrExchange: any) => {
                   vote: (_result, _args, cache, _info) => {
                      invalidateActivityFeed(cache)
                      invalidateMe(cache)
-                  }
-               }
+                  },
+                  savePublicCollection: (_result, _args, cache, _info) => {
+                     invalidateCollections(cache)
+                  },
+               },
             },
             keys: {
                ActivityFeedResponse: () => null
