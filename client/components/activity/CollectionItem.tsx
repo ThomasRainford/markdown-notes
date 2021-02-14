6374dd96a8d1fffc80dc6f3b5b0542cae6f6830d
Thomas Rainford <thomas@rainfords.net>
2021-02-15 10:49:31 +1300

    Open selected collection accordion.

--- client/components/activity/CollectionItem.tsx
+++ client/components/activity/CollectionItem.tsx
@@ -1,19 +1,30 @@
-import { Center, Divider, Flex, Icon, ListIcon, ListItem, Text, Tooltip } from '@chakra-ui/react'
+import { Flex, ListIcon, ListItem, Text, Tooltip } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import React from 'react'
-import { MdLock, MdLockOpen, MdThumbUp } from 'react-icons/md'
-import { Collection } from '../../generated/graphql'
+import { MdLock, MdLockOpen } from 'react-icons/md'
+import { UseQueryState } from 'urql'
+import { Collection, MeQuery, User } from '../../generated/graphql'
 import CollectionInfo from './CollectionInfo'
 
 interface Props {
    collection: Collection
+   user: UseQueryState<MeQuery, object>
+   index: number
 }
 
-const CollectionItem: React.FC<Props> = ({ collection }) => {
+const CollectionItem: React.FC<Props> = ({ collection, user, index }) => {
+
+   const router = useRouter()
+
    return (
       <ListItem key={collection.id}>
          <Tooltip hasArrow label="Open Collection" bg="#379683">
             <Flex direction="column" bg="#8EE4AF" p="0.5em" border="1px" borderColor="#EDF5E1" borderRadius="md"
                _hover={{ borderColor: "#05386B" }}
+               onClick={() => {
+                  localStorage.setItem('collectionIndex', index.toString())
+                  router.push(`/my-notes/${user.data?.me?.username}`)
+               }}
             >
                <Flex align="center">
                   <ListIcon as={collection.visibility === 'public' ? MdLockOpen : MdLock} />
