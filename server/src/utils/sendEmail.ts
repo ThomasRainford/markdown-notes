42f0788cb5e501bc506b2c566b253b4db3d0258b
Thomas Rainford <thomas@rainfords.net>
2021-02-19 13:03:10 +1300

    Create function for sending an email to a user.

---
+++ server/src/utils/sendEmail.ts
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+// async..await is not allowed in global scope, must use a wrapper
+export async function sendEmail(to: string, html: string): Promise<void> {
+   // Generate test SMTP service account from ethereal.email
+   // Only needed if you don't have a real mail account for testing
+   // let testAccount = await nodemailer.createTestAccount();
+   // console.log("testAccount", testAccount);
+
+   // create reusable transporter object using the default SMTP transport
+   const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+         user: 'wilhelm31@ethereal.email',
+         pass: 'xGPeQ22nD8gjHXkgG2'
+      }
+   });
+
+   // send mail with defined transport object
+   const info = await transporter.sendMail({
+      from: '"Markdown Note" <markdown-notes@app.com>', // sender address
+      to: to, // list of receivers
+      subject: "Change password", // Subject line
+      html,
+   });
+
+   console.log("Message sent: %s", info.messageId);
+   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
