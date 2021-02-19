a466704d7dbe47ae2f306a50472b9bef810248a2
Thomas Rainford <thomas@rainfords.net>
2021-02-19 16:20:11 +1300

    Add heading and lists.

--- client/utils/snippets.ts
+++ client/utils/snippets.ts
@@ -1,19 +1,53 @@
+interface Heading {
+   h1: string
+   h2: string
+   h3: string
+   h4: string
+   h5: string
+   h6: string
+}
+
 interface Emphasis {
    italics: string
    bold: string
    strikeThrough: string
 }
 
+interface Lists {
+   ordered: string
+   orderedSublist: string
+   unordered: string
+   unorderedSublist: string
+   paragraph: string
+}
+
 export interface Snippets {
+   heading: Heading
    emphasis: Emphasis
    link: string
+   lists: Lists
 }
 
 export const snippets: Snippets = {
+   heading: {
+      h1: "# ",
+      h2: "## ",
+      h3: "### ",
+      h4: "#### ",
+      h5: "##### ",
+      h6: "###### ",
+   },
    emphasis: {
       italics: "* *",
       bold: "** **",
       strikeThrough: "~~ ~~"
    },
-   link: '[]()'
+   link: '[]()',
+   lists: {
+      ordered: "1. ",
+      orderedSublist: "⋅⋅1. ",
+      unordered: "* ",
+      unorderedSublist: "⋅⋅* ",
+      paragraph: "⋅⋅⋅ "
+   }
 }
