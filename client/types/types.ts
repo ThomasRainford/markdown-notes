3792f50bbae2c2475c82df39407fd94d90f5a6df
Thomas Rainford <thomas@rainfords.net>
2021-02-22 17:53:13 +1300

    Move snippet types to type files.

--- client/types/types.ts
+++ client/types/types.ts
@@ -8,3 +8,57 @@ export interface NoteLocation {
 export interface ExactNoteLocation {
    noteLocation: NoteLocation & { note: Note }
 }
+
+// Snippets types.
+interface Heading {
+   h1: string
+   h2: string
+   h3: string
+   h4: string
+   h5: string
+   h6: string
+}
+
+interface Emphasis {
+   italics: string
+   bold: string
+   strikeThrough: string
+}
+
+interface Lists {
+   ordered: string
+   orderedSublist: string
+   unordered: string
+   unorderedSublist: string
+   paragraph: string
+}
+
+interface Images {
+   inline: string
+}
+
+interface Code {
+   inline: string
+   block: string
+}
+
+export interface TableDimensions {
+   rows: number
+   columns: number
+}
+
+interface Table {
+   create: (dimensions: TableDimensions) => string
+}
+export interface Snippets {
+   heading: Heading
+   emphasis: Emphasis
+   link: string
+   lists: Lists
+   images: Images
+   code: Code
+   table: Table
+   blockquotes: string
+   htmlElement: string
+   horizontalRule: string
+}
--- client/utils/snippets.ts
+++ client/utils/snippets.ts
@@ -1,55 +1,5 @@
-interface Heading {
-   h1: string
-   h2: string
-   h3: string
-   h4: string
-   h5: string
-   h6: string
-}
+import { Snippets } from "../types/types"
 
-interface Emphasis {
-   italics: string
-   bold: string
-   strikeThrough: string
-}
-
-interface Lists {
-   ordered: string
-   orderedSublist: string
-   unordered: string
-   unorderedSublist: string
-   paragraph: string
-}
-
-interface Images {
-   inline: string
-}
-
-interface Code {
-   inline: string
-   block: string
-}
-
-export interface TableDimensions {
-   rows: number
-   columns: number
-}
-
-interface Table {
-   create: (dimensions: TableDimensions) => string
-}
-interface Snippets {
-   heading: Heading
-   emphasis: Emphasis
-   link: string
-   lists: Lists
-   images: Images
-   code: Code
-   table: Table
-   blockquotes: string
-   htmlElement: string
-   horizontalRule: string
-}
 
 export const snippets: Snippets = {
    heading: {
